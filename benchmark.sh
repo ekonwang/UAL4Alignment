e3463538a0c77ae8b3d21d62358e35925773cd0b
root <g1547246193@gmail.com>
2024-01-04 02:43:44 +0000

    update

--- benchmark.sh
+++ benchmark.sh
@@ -4,13 +4,18 @@ LORA_LIMA_E20=/workspace/lit-llama/out/lora/lima/sft_lima_lora_sctx-512_multi-di
 LORA_ADA_LIMA_E20=/workspace/lit-llama/out/lora/lima/sft_ada-lima_lora_sctx-512_ls-0.10_23-12-28-08-32-26/iter-020479-ckpt.pth
 
 
-# python benchmark/benchmark_mc.py --data_dir "ARC" --shot_num 5
-# python benchmark/benchmark_mc.py --data_dir "TruthfulQA"
-python benchmark/benchmark_mc.py --data_dir "MMLU" --shot_num 5
+for shot_num in 0 1 2 3 4 5 
+do
+    # python benchmark/benchmark_mc.py --data_dir "ARC" --shot_num $shot_num
+    # python benchmark/benchmark_mc.py --data_dir "TruthfulQA" --shot_num $shot_num
+    # python benchmark/benchmark_mc.py --data_dir "MMLU" --shot_num $shot_num
+    python benchmark/benchmark_mc.py --data_dir "HellaSwag" --shot_num $shot_num 
+done 
+
 
 for lora_path in $LORA_ALPACA $LORA_LIMA_E15 $LORA_LIMA_E20 $LORA_ADA_LIMA_E20
 do
     # python benchmark/benchmark_mc.py --data_dir "ARC" --lora_path $lora_path --shot_num 5
     # python benchmark/benchmark_mc.py --data_dir "TruthfulQA" --lora_path $lora_path
-    python benchmark/benchmark_mc.py --data_dir "MMLU" --lora_path $lora_path --shot_num 5
+    # python benchmark/benchmark_mc.py --data_dir "MMLU" --lora_path $lora_path --shot_num 5
 done
